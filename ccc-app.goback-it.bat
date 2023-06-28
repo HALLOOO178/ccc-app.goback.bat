473f5dfc3e089915989706e6759f43ffc6886165
Hallo <118673681+HALLOOO178@users.noreply.github.com>
2023-06-28 09:28:25 +0200

    Add files via upload

---
+++ ccc-app.goback-it.bat
@@ -0,0 +1,28 @@
+@echo off
+:1
+if not exist X:\old.bat goto cn
+cls
+echo Tornare a "Winbrew.bat"?
+echo (Y/N)
+set /p input=
+if /i %input%== y goto cn
+if /i %input%==n goto back
+goto 1
+
+:back
+X:
+cd \
+cls
+old.bat
+echo unable to boot
+pause
+goto 1
+
+:cn
+X:
+cd \
+cls
+Winbrew.bat
+echo unable to boot
+pause
+goto 1
